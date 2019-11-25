d593e6fb96cdbb48a0f997dae62eaffcbd1d88e2
Web Developer <webdeveloper@Webs-MacBook-Pro.local>
2019-11-25 12:34:17 -0800

    first commit

---
+++ dbschema.js
@@ -0,0 +1,64 @@
+let db = {
+    users: [
+      {
+        userId: 'dh23ggj5h32g543j5gf43',
+        email: 'user@email.com',
+        handle: 'user',
+        createdAt: '2019-11-12T10:59:52.798Z',
+        imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+        bio: 'Hello, my name is user, nice to meet you',
+        website: 'https://user.com',
+        location: 'Bellevue, WA, US'
+      }
+    ],
+    screams: [
+      {
+        userHandle: 'user',
+        body: 'This is a sample scream',
+        createdAt: '2019-11-12T10:59:52.798Z',
+        likeCount: 5,
+        commentCount: 3
+      }
+    ],
+    comments: [
+      {
+        userHandle: 'user',
+        screamId: 'kdjsfgdksuufhgkdsufky',
+        body: 'nice one mate!',
+        createdAt: '2019-11-12T10:59:52.798Z'
+      }
+    ],
+    notifications: [
+      {
+        recipient: 'user',
+        sender: 'john',
+        read: 'true | false',
+        screamId: 'kdjsfgdksuufhgkdsufky',
+        type: 'like | comment',
+        createdAt: '2019-11-12T10:59:52.798Z'
+      }
+    ]
+  };
+  const userDetails = {
+    // Redux data
+    credentials: {
+      userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-11-12T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://user.com',
+      location: 'Bellevue, WA, US'
+    },
+    likes: [
+      {
+        userHandle: 'user',
+        screamId: 'hh7O5oWfWucVzGbHH2pa'
+      },
+      {
+        userHandle: 'user',
+        screamId: '3IOnFoQexRcofs5OhBXO'
+      }
+    ]
+  };
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAFHYEMkO3Gd445KpKoo4HV0dF9PxCVsVc",
+    authDomain: "socialapp-8ef27.firebaseapp.com",
+    databaseURL: "https://socialapp-8ef27.firebaseio.com",
+    projectId: "socialapp-8ef27",
+    storageBucket: "socialapp-8ef27.appspot.com",
+    messagingSenderId: "30458479514",
+    appId: "1:30458479514:web:4ef0201b219acfbe6fb023",
+    measurementId: "G-N2D4M7KP2W"
+  };
